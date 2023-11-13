99b97cbcac382d6808a2609c9d2628d4b65944ce
Cristian Bula Casa <c.bula@gotuuri.com>
2023-11-12 22:59:08 -0500

    another cors test 4

--- src/app.ts
+++ src/app.ts
@@ -9,8 +9,10 @@ config();
 const app = express();
 
 //middlewares
-// app.use(cors({origin: "http://localhost:3000", credentials: true}))
-app.use(cors({origin: "*", credentials: true}))
+app.use(cors({
+    origin: ["http://localhost:3000", "https://mind-well-elauy2015.vercel.app"],
+    credentials: true,
+  }));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
